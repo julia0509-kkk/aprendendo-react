0afa92d0cc41f44b4c59ec554d07a77dcc5ef7f9
Julia Miria <juliamiriafonseca@gmail.com>
2024-09-27 17:23:34 -0300

    correçao da tela de tarefa

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -2,15 +2,18 @@ import { ChevronLeftIcon } from "lucide-react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 function TaskPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const title = searchParams.get("title");
   const description = searchParams.get("description");
   return (
     <div className="h-screen w-screen bg-slate-500 p-6">
-      <div className="w-[500px] space-y-4">
+      <div className="w-[500px] mx-auto space-y-4">
         <div className="flex justify-center relative mb-6">
-          <button onClick={() => navigate(-1)} className="absolute left-0 top-0 bottom-0 text-slate-100">
+          <button
+            onClick={() => navigate(-1)}
+            className="absolute left-0 top-0 bottom-0 text-slate-100"
+          >
             <ChevronLeftIcon />
           </button>
           <h1 className="text-3xl text-slate-100 font-bold text-center">
